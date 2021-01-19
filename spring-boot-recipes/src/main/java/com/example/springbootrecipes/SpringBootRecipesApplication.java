bef38f182a3d5b5e21f78139635eb47cdf24d0a8
Gaurav Khanna <iamgauravkhanna@gmail.com>
2021-01-19 20:37:02 +0530

    changes

---
+++ spring-boot-recipes/src/main/java/com/example/springbootrecipes/SpringBootRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRecipesApplication.class, args);
+	}
+
+}
